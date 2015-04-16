8051022dcd2c0ff380f0cd3b2963ac167116a5ec
Chris Oyler <christopher.oyler@gmail.com>
2015-04-15 20:52:13 -0700

    drawing cards with photos

--- client/index.js
+++ client/index.js
@@ -1,2 +1,38 @@
 'use strict';
-console.log('apollo -> online');
+
+$(document).ready(init);
+
+function init(){
+  $('body').on('click', '#draw', generateTiles);
+  
+}
+// $('#fetch').on('click', function(){
+//   var value = $('#input').val()
+//   var url = 'https://api.github.com/users/'+value+;
+//
+//   $.getJSON(url, function(){
+//
+//   })
+// })
+
+var gitIds = ['samerbuna', 'IAmEddieDean', 'dhh'];
+
+function generateTiles(){
+  // var url = 'https://api.github.com/users/'+users;
+  gitIds.forEach(function(users){
+    var url = 'https://api.github.com/users/'+users;
+    $.getJSON(url, function(response){
+      console.log(response.avatar_url);
+      var $div = $('<div>');
+      $div.attr('class', 'col-md-4');
+      $div.attr('id', users)
+      $div.text(response.name);
+      $('.row').append($div);
+      var $img = $('<div>');
+      $img.addClass('image');
+      $img.css('background-image', 'url("'+response.avatar_url+'")');
+      $($div).append($img);
+  });
+  });
+}
+  
