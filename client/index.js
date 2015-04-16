5dd5712c9dead54a421de24713752b4376ad2bb0
AgileLabs <samer@agilelabs.com>
2015-04-16 16:00:58 -0700

    #6 Do code cleanup | No review needed

--- client/index.js
+++ client/index.js
@@ -2,28 +2,16 @@
 
 $(document).ready(init);
 
-function init(){
+function init() {
   generateTiles();
-  // $('body').on('click', '#draw', generateTiles);
-  
 }
-// $('#fetch').on('click', function(){
-//   var value = $('#input').val()
-//   var url = 'https://api.github.com/users/'+value+;
-//
-//   $.getJSON(url, function(){
-//
-//   })
-// })
 
 var gitIds = ['samerbuna', 'IAmEddieDean', 'dhh'];
 
-function generateTiles(){
-  // var url = 'https://api.github.com/users/'+users;
+function generateTiles() {
   gitIds.forEach(function(users){
     var url = 'https://api.github.com/users/'+users;
     $.getJSON(url, function(response){
-      console.log(response.avatar_url);
       var $div = $('<div>');
       $div.attr('class', 'col-md-4');
       $div.attr('id', users)
@@ -33,7 +21,6 @@ function generateTiles(){
       $img.addClass('image');
       $img.css('background-image', 'url("'+response.avatar_url+'")');
       $($div).append($img);
-  });
+    });
   });
 }
-  
